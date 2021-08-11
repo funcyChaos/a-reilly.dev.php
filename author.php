d57e810f268e95c105398c9ca314c97fc1d3127d
Austin Reilly <areilly@alphaworks.tech>
2021-08-11 10:25:23 -0700

    added an author.php it doesn't work the way I'd like it to, but it at least exists now so that you don't just see the categories and no authors if you click on an authors name xP work in progress eh

---
+++ author.php
@@ -0,0 +1,46 @@
+<? get_header();?>
+
+	<div class="sidebar-cell" id="lcell">
+		<? wp_list_authors();?>
+	</div>
+
+	<main class="main-content-cell column justify-start p0">
+
+	<?
+		while (have_posts())
+		{
+			the_post();?>
+
+			<div class="blog-preview">
+				<div class="flex">
+					<a href="<? the_permalink();?>">
+						<h1>&lt<? the_title();?>&gt</h1>
+					</a>
+				</div>
+				<div class="flex justify-space-around wrap bp-col">
+					<div class="flex column justify-space-between ex-size">
+						<div class="blog-excerpt">
+							<? the_excerpt();?>
+							<a href="<? the_permalink();?>">Continue reading</a>
+						</div>
+						<div class="blog-data">
+							<p>author:<? the_Author_posts_link();?></p>
+							<p>published:<? the_time('d.m.Y');?></p>
+							<p>categorized:<? echo get_the_category_list();?></p>
+						</div>
+					</div>
+					<div class="column">
+						<? the_post_thumbnail('full'); ?>
+					</div>
+				</div>
+			</div>
+
+			<? } ?>
+		<div class="flex justify-center">
+			<? echo paginate_links();?>
+		</div>
+
+	</main>
+
+
+<? get_footer();?>
