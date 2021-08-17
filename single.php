7f0212e67f8609456aecbf6a0db75e738752303c
Austin Reilly <areilly@alphaworks.tech>
2021-08-17 11:53:05 -0700

    just kidding, I forgot a mistake on single.php

--- single.php
+++ single.php
@@ -17,10 +17,7 @@
 				<p>author:<? the_Author_posts_link();?></p>
 				<p>published:<? the_time('d.m.Y');?></p>
 				<p>categorized:<? echo get_the_category_list();?></p>
-			</div>
-
-				
-				
+			</div>				
 		<? }
 
 		if (comments_open() || get_comments_number())
