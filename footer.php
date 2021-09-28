654ccde06885b74c5fb571447982114b5ca70bdf
Austin Reilly <areilly@alphaworks.tech>
2021-09-28 15:53:29 -0700

    added conditional for second icon

--- footer.php
+++ footer.php
@@ -4,12 +4,17 @@
 					<!-- <i title="tinker-dot-bin" class="fas fa-code-branch fa-5x"></i> -->
 					<? echo get_theme_mod('custom_icon_1');?>
 				</a>
-
-				<a href="<? echo get_theme_mod('icon_2_link');?>" target="_blank">
-					<!-- https://www.instagram.com/austin_reilly_93/ -->
-					<!-- <i title="@austin_reilly_93" class="fas fa-hashtag fa-5x"></i> -->
-					<? echo get_theme_mod('custom_icon_2');?>
-				</a>
+				<?
+				if(get_theme_mod('custom_icon_2') != '')
+				{
+					?>
+					<a href="<? echo get_theme_mod('icon_2_link');?>" target="_blank">
+						<!-- https://www.instagram.com/austin_reilly_93/ -->
+						<!-- <i title="@austin_reilly_93" class="fas fa-hashtag fa-5x"></i> -->
+						<? echo get_theme_mod('custom_icon_2');?>
+					</a>
+					<?
+				} ?>
 			</footer>
 		</div>
 		<? wp_footer();?>
